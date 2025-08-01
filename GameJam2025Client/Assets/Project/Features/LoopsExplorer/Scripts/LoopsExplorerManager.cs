75db1bb7d09adf8cb177669138ab9a8bec14b9fa
vovasazonove <152857638+vovasazonove@users.noreply.github.com>
2025-08-01 20:22:08 +0300

    Updat GameManager scene

---
+++ GameJam2025Client/Assets/Project/Features/LoopsExplorer/Scripts/LoopsExplorerManager.cs
@@ -0,0 +1,15 @@
+using Project.Core.Scripts;
+using UnityEngine;
+
+namespace Project.Features.LoopsExplorer.Scripts
+{
+    public class LoopsExplorerManager : SingletonBehaviour<LoopsExplorerManager>
+    {
+        [SerializeField] private float _radiusFinger = 10f;
+        
+        private void Update()
+        {
+            
+        }
+    }
+}
