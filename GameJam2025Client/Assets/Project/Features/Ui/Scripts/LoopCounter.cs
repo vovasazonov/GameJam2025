5acf81f778e0faec675ee993fc80fc65aedf9034
matikkk231 <115667337+matikkk231@users.noreply.github.com>
2025-08-02 23:48:51 +0300

    wdfs

--- GameJam2025Client/Assets/Project/Features/Ui/Scripts/LoopCounter.cs
+++ GameJam2025Client/Assets/Project/Features/Ui/Scripts/LoopCounter.cs
@@ -1,11 +1,14 @@
 using System;
 using Project.Features.LoopsExplorer.Scripts;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class LoopCounter : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI loopFoundDisplay;
+    [SerializeField] private GameObject levelChoserMenu;
+    [SerializeField] private GameObject levelMenu;
 
     void Start()
     {
@@ -16,5 +19,11 @@ public class LoopCounter : MonoBehaviour
     {
         loopFoundDisplay.text = Convert.ToString(LoopsExplorerManager.Instance.FoundLoops) + " found from" + " " +
                                 LoopsExplorerManager.Instance.TotalLoops;
+
+        if (LoopsExplorerManager.Instance.FoundLoops == LoopsExplorerManager.Instance.TotalLoops)
+        {
+            levelMenu.SetActive(false);
+            levelChoserMenu.SetActive(true);
+        }
     }
 }
