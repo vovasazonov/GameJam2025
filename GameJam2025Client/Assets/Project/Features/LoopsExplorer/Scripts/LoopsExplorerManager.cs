fcabccd58165782c1b144bd54446a3ff59beb836
vovasazonove <152857638+vovasazonove@users.noreply.github.com>
2025-08-02 18:08:06 +0300

    Add line explorer

--- GameJam2025Client/Assets/Project/Features/LoopsExplorer/Scripts/LoopsExplorerManager.cs
+++ GameJam2025Client/Assets/Project/Features/LoopsExplorer/Scripts/LoopsExplorerManager.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Project.Core.Scripts;
 using Project.Features.Input.Scripts;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace Project.Features.LoopsExplorer.Scripts
 {
@@ -10,6 +12,8 @@ namespace Project.Features.LoopsExplorer.Scripts
     {
         [SerializeField] private float _radiusFinger = 10f;
         [SerializeField] private float _matchThreshold = 0.6f;
+        [SerializeField] private LineRenderer _touchLineRenderer;
+        [SerializeField] private LineRenderer _loopPrefab;
 
         private readonly Dictionary<int, List<Vector2>> _allLoopsById = new();
         private readonly HashSet<int> _foundLoopsIds = new();
@@ -18,9 +22,26 @@ namespace Project.Features.LoopsExplorer.Scripts
         private readonly List<Vector2> _touchedPoints = new();
         private bool _isDown;
 
+        private readonly List<LineRenderer> _foundLoops = new();
+
+        public event Action<int> FoundLoop;
         public int TotalLoops => _allLoopsById.Count;
         public int FoundLoops => _foundLoopsIds.Count;
         public IEnumerable<int> AllFoundLoopsIds() => _allLoopsById.Keys;
+
+        // TODO: remove
+        // private void Start()
+        // {
+        //     _foundLoops.Add(Object.Instantiate(_loopPrefab));
+        //     var level = LevelDatabase.Instance.GetLevel(3);
+        //     InitializeExplorer(level.points, level.cycles);
+        //     _foundLoops[0].positionCount = level.points.Count;
+        //     for (var i = 0; i < level.points.Count; i++)
+        //     {
+        //         var point = level.points[i];
+        //         _foundLoops[0].SetPosition(i, point);
+        //     }
+        // }
 
         public void InitializeExplorer(List<Vector2> fullLine, List<List<Vector2>> loops)
         {
@@ -51,15 +72,22 @@ namespace Project.Features.LoopsExplorer.Scripts
                     _touchedPoints.Clear();
                 }
 
-                var position = InputManager.Instance.PointPosition;
-                var closestPoint = GetClosestPointOnLine(position);
+                var inputPosition = InputManager.Instance.PointPosition;
+                var cameraZ = -1 * Camera.main.transform.position.z;
+                var currentPosition = Camera.main.ScreenToWorldPoint(new Vector3(inputPosition.x, inputPosition.y, cameraZ));
+                currentPosition.z = 0;
+                
+                var closestPoint = GetClosestPointOnLine(new Vector2(currentPosition.x, currentPosition.y));
 
                 if (closestPoint.HasValue)
                 {
                     var point = closestPoint.Value;
-                    if (_touchedPoints.Count == 0 || _touchedPoints.Last() != point)
+                    if (_touchedPoints.Count == 0 || _touchedPoints[^1] != point)
                     {
                         _touchedPoints.Add(point);
+
+                        _touchLineRenderer.positionCount++;
+                        _touchLineRenderer.SetPosition(_touchedPoints.Count - 1, point);
                     }
                 }
             }
@@ -72,6 +100,8 @@ namespace Project.Features.LoopsExplorer.Scripts
 
         private void OnTouchEnd()
         {
+            _touchLineRenderer.positionCount = 0;
+            
             if (_touchedPoints.Count < 3)
                 return;
 
@@ -87,7 +117,8 @@ namespace Project.Features.LoopsExplorer.Scripts
                 if (similarity >= _matchThreshold)
                 {
                     _foundLoopsIds.Add(kvp.Key);
-                    Debug.Log($"Loop {kvp.Key} found!");
+                    Debug.Log($"Loop id:{kvp.Key} found! Total loops: {TotalLoops}, Found loops: {FoundLoops}");
+                    FoundLoop?.Invoke(kvp.Key);
                     break;
                 }
             }
