574bcb8a35f5cf11113e727dcc0e6d6d838468d2
vovasazonove <152857638+vovasazonove@users.noreply.github.com>
2025-08-02 17:12:47 +0300

    Update LoopsExplorerManager.cs

--- GameJam2025Client/Assets/Project/Features/LoopsExplorer/Scripts/LoopsExplorerManager.cs
+++ GameJam2025Client/Assets/Project/Features/LoopsExplorer/Scripts/LoopsExplorerManager.cs
@@ -12,17 +12,26 @@ namespace Project.Features.LoopsExplorer.Scripts
         [SerializeField] private float _radiusFinger = 10f;
         [SerializeField] private float _matchThreshold = 0.8f;
 
+        private readonly Dictionary<int, List<Vector2>> _allLoopsById = new Dictionary<int, List<Vector2>>();
         private readonly HashSet<int> _foundLoopsIds = new HashSet<int>();
-        private List<Vector2> _fullLine = new List<Vector2>();
-        private List<List<Vector2>> _toFindLoops = new List<List<Vector2>>();
+        private readonly List<Vector2> _fullLine = new List<Vector2>();
 
         private bool _isDown;
 
+        public int TotalLoops => _allLoopsById.Count;
+        public int FoundLoops => _foundLoopsIds.Count;
+        
         public void InitializeExplorer(List<Vector2> fullLine, List<List<Vector2>> loops)
         {
-            _fullLine = fullLine;
-            _toFindLoops = loops;
+            _fullLine.Clear();
+            _allLoopsById.Clear();
             _foundLoopsIds.Clear();
+
+            _fullLine.AddRange(fullLine);
+            for (var i = 0; i < loops.Count; i++)
+            {
+                _allLoopsById.Add(i, loops[i]);
+            }
         }
         
         private void Update()
