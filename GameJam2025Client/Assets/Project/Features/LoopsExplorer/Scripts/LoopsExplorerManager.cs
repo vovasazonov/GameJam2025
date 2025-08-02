e316ca7ceb678412e069b038c7e89c4de0853974
vovasazonove <152857638+vovasazonove@users.noreply.github.com>
2025-08-02 17:09:56 +0300

    Update LoopsExplorerManager.cs

--- GameJam2025Client/Assets/Project/Features/LoopsExplorer/Scripts/LoopsExplorerManager.cs
+++ GameJam2025Client/Assets/Project/Features/LoopsExplorer/Scripts/LoopsExplorerManager.cs
@@ -1,4 +1,8 @@
+using System.Collections.Generic;
+using System.Linq;
 using Project.Core.Scripts;
+using Project.Features.Input.Scripts;
+using Project.Features.LineCalculation.Scripts;
 using UnityEngine;
 
 namespace Project.Features.LoopsExplorer.Scripts
@@ -6,10 +10,39 @@ namespace Project.Features.LoopsExplorer.Scripts
     public class LoopsExplorerManager : SingletonBehaviour<LoopsExplorerManager>
     {
         [SerializeField] private float _radiusFinger = 10f;
+        [SerializeField] private float _matchThreshold = 0.8f;
+
+        private readonly HashSet<int> _foundLoopsIds = new();
+        private List<Vector2> _fullLine = new();
+        private List<List<Vector2>> _toFindLoops = new();
+
+        private bool _isDown;
+
+        public void InitializeExplorer(List<Vector2> fullLine, List<List<Vector2>> loops)
+        {
+            _fullLine = fullLine;
+            _toFindLoops = loops;
+            _foundLoopsIds.Clear();
+        }
         
         private void Update()
         {
-            
+            if (InputManager.Instance.IsPointDown)
+            {
+                if (!_isDown)
+                {
+                    _isDown = true;
+                }
+            }
+            else if (_isDown)
+            {
+                _isDown = false;
+                OnTouchEnd();
+            }
+        }
+
+        private void OnTouchEnd()
+        {
         }
     }
 }
